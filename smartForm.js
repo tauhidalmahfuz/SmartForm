e9c0406aff9416915a9c7927ac449fa644749cab
cis101 <cis101@secs.iub.edu.bd>
2017-10-09 09:38:25 +0600

    assignment 7

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,48 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:"",
+  email:"",
+  html:{likes:[], dislikes:[]},
+  css: {likes:[], dislikes:[]},
+  js:{likes:[], dislikes:[]},
+  strengths:{html:"",css:"",js:""},
+  currentQuestion:"welcome"
+};
+$("#startbtn").click(function(event){
+  $("#welcome").hide();
+  $("#q1").show();
+});
+
+$("#name").change(function(event){
+  console.log($("#name").val());
+});
+$("#email").change(function(event){
+  //validation of email is needed;
+  console.log($("#email").val());
+  //if the fields are empty button should be empty;
+  $("#nextbtn").prop("disabled",false);
+});
+$("#nextbtn").click(function(event){
+  userData.name=$("#name").val();
+  userData.email = $("#email").val();
+  $("#q1").hide();
+  $("#q2").show();
+});
+$("#htmlbtn").click(function(event){
+  $("#q2").hide();
+  $("#q2a").show();
+});
+$("#cssbtn").click(function(event){
+  $("#q2").hide();
+  $("#q2b").show();
+});
+$("#jsbtn").click(function(event){
+  $("#q2").hide();
+  $("#q2c").show();
+});
+
+$(".previousbtn").click(function(event){
+  $("#q2").show();
+  $(this).parent().parent().parent().hide();
+});
