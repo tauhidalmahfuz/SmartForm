4875024e6a4e7dd81396d91624761d1b8590923a
cis101 <cis101@secs.iub.edu.bd>
2017-10-11 09:31:26 +0600

    assignment 8

--- smartForm.js
+++ smartForm.js
@@ -1,14 +1,25 @@
 // Your jQuery goes here
-
-var userData = {
-  name:"",
+var userData={};
+if(localStorage.getItem('userData')){
+  userData=JSON.parse(localStorage.getItem('userData'));
+  $('.question').hide();
+  $('#'+userData.currentQuestion).show();
+  $("#name").val(userData.name);
+  $("#email").val(userData.email);
+  $("#nextbtn").prop("disabled",false);
+}
+else {
+  userData = {
+  name:"test",
   email:"",
   html:{likes:[], dislikes:[]},
   css: {likes:[], dislikes:[]},
   js:{likes:[], dislikes:[]},
   strengths:{html:"",css:"",js:""},
   currentQuestion:"welcome"
-};
+  };
+  localStorage.setItem('userData',JSON.stringify(userData));
+}
 $("#startbtn").click(function(event){
   $("#welcome").hide();
   $("#q1").show();
@@ -26,6 +37,8 @@ $("#email").change(function(event){
 $("#nextbtn").click(function(event){
   userData.name=$("#name").val();
   userData.email = $("#email").val();
+  userData.currentQuestion = "q1";
+  localStorage.setItem('userData',JSON.stringify(userData));
   $("#q1").hide();
   $("#q2").show();
 });
